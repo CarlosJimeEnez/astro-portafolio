be1faea91fb364bef9e370d9a565e25917d61995
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-10-04 11:59:38 -0600

    Add initial project configuration files and folders

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'text': 'var(--text)',
+        'background': 'var(--background)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'accent': 'var(--accent)',
+       },
+       fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+       }
+    },
+  },
+  plugins: [],
+
+}
+
