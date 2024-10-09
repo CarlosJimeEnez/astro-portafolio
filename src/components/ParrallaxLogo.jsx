59caf47f35f8879db9bb9ffee58abb51fcab3671
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-10-09 10:28:25 -0600

    Refactor Astro config and HeroSection component

--- src/components/ParrallaxLogo.jsx
+++ src/components/ParrallaxLogo.jsx
@@ -1,18 +1,11 @@
-import React, { useEffect } from 'react';
 import SimpleParallax from "simple-parallax-js";
 
-const ParallaxLogo = () => {
-  useEffect(() => {
-    new SimpleParallax(document.querySelector("img"), {
-      scale: 1.5,
-      orientation: "up",
-      delay: .6,
-      transition: "cubic-bezier(0,0,0,1)",
-    });
-  }, []);
-  <SimpleParallax>
-    <img src={"Logo.svg"} alt={"image"} />
-  </SimpleParallax>
-};
+function ParallaxLogo() {
+  return(
+    <SimpleParallax overflow="true">
+      <img src="src\assets\Logo.svg" alt='logo'/>
+    </SimpleParallax>
+  )
+}
 
 export default ParallaxLogo;
